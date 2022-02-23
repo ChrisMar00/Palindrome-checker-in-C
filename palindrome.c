64620f5a5e6e38e3c80a56f680f2f13bb9f90e67
Christian M <chrismarotta00@gmail.com>
2022-02-23 15:38:59 +0100

    Update palindrome.c

--- palindrome.c
+++ palindrome.c
@@ -16,10 +16,19 @@ int stringLength(char *s){
     return ps - s;//Return the the distance between the final ps position and the starting position s
 }
 
+//Checks if a character is upperCase, if so it changes it to lowerCase
+void checkUpperCase(char *s){
+    if(*s >= 65 && *s <= 90){
+        *s += 32;
+    }
+}
+
 //Checks if a string is a palindrome or not
 int palindrome(char *s){
     char *ps = s, *pf = (s + stringLength(s) - 1);//Sets the *ps to the first character and *pf to the last one
     for(;(ps - pf) < stringLength(s) / 2;ps++, pf--){//Cycles through half string, incrementing ps and decrementing pf
+        checkUpperCase(ps);
+        checkUpperCase(pf);
         if(*ps != *pf) return 0;//If *ps != *pf then one of the characters in the first half of the string is different to the corrisponding one on the other half of the string
     }
     return 1;
@@ -27,6 +36,9 @@ int palindrome(char *s){
 
 int main(int argc, char *argv[]){
     if(argc != 2) printInvalidFormatError(argv[0]);
-    printf("%d", palindrome(argv[1]));
+
+    if(palindrome(argv[1]) == 1) printf("%s is palindrome!\n", argv[1]);
+    else printf("%s isn't palindrome!\n", argv[1]);
+
     return 0;
 }
