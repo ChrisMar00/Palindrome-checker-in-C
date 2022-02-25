4bb23de10a97dcf5c0abd2dc0cdced8274df66fb
Christian Marotta <chrismarotta00@gmail.com>
2022-02-25 02:44:12 +0100

    Update palindrome.c

--- palindrome.c
+++ palindrome.c
@@ -6,7 +6,7 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-//Function prototypes
+//Functions prototypes
 void printInvalidFormatError(char *prog);
 void checkUpperCase(char *s);
 int palindrome(char *s);
